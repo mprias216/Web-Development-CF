05ca643abb4c8ba0003084b601cf5f5c7aed83e9
mprias216 <74328348+mprias216@users.noreply.github.com>
2021-01-30 19:11:10 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
+let name = 'Clarity Fitness';
+const greeting = `Good morning! Welcome to ${name}, What type of Workout would you want for today?`;
+let numberOfExercises = 50;
+numberOfExercises = 3;
 
+const typesOfExercises = ['Pushups', 'Jumping Jacks', 'Wall Planks'];
+
+todayIsWorkoutDay = false;
+
+let WorkoutType = {
+  workout: 'HITT',
+  minutes: 30
+}
